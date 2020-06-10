bdcc61b6c89658272ad8701f075ddac0082e3424
Emmanuel <akhigbemmanuel@gmai.com>
2020-06-10 03:24:55 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd9SI3fMZxxZg8Y53Rt69r2OSBKEK5H-4",
+  authDomain: "evernote-clone-one.firebaseapp.com",
+  databaseURL: "https://evernote-clone-one.firebaseio.com",
+  projectId: "evernote-clone-one",
+  storageBucket: "evernote-clone-one.appspot.com",
+  messagingSenderId: "242726794196",
+  appId: "1:242726794196:web:537f5307bd472b93c85c41",
+  measurementId: "G-4N1LKGP43V"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
