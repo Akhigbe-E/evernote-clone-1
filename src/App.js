3fb05e6bb4b9cda8860ce7bf7970832dc5236019
Emmanuel <akhigbemmanuel@gmai.com>
2020-06-10 04:10:35 +0100

    Setting up project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const firebase = require('firebase')
+
 const App = () => {
+  const initialState = {
+    selectedNoteIndex: null,
+    selectedNote: null,
+    notes: null
+  }
+  const [state, setstate] = useState(initialState)
+
+  useEffect(() => {
+    firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+      const notes = serverUpdate.docs.map(_doc => {
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      })
+      console.log(notes)
+      setstate({ notes })
+    })
+    // return () => {
+    //   cleanup
+    // }
+  }, [])
+
   return (
     <div >
       Hello World!
